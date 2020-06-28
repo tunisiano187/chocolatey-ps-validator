ed0e32a13b784bdf45bd17a1479a9511e2de4ecf
Tunisiano187 <fabian@bowlman.org>
2020-06-28 18:56:09 +0200

    First commit

---
+++ Setup.ps1
@@ -0,0 +1,20 @@
+# Define default settings for the manifest
+$moduleSettings = @{
+    ModuleVersion = '0.0.1'
+    Author = 'tunisiano'
+    CompanyName = ''
+    Copyright = '@tunisiano187'
+    Description = 'This package will try to give an easy way to check AU package before pack and publishing'
+    FunctionsToExport = @()
+    AliasesToExport = @()
+    PrivateData = @{
+        PSData = @{
+            ProjectUri = 'https://github.com/tunisiano187/chocolatey-ps-validator'
+            RequireLicenseAcceptance = $false
+        }
+    }
+    HelpInfoURI = 'https://gitter.im/Tunisiano18/community?utm_source=share-link&utm_medium=link&utm_campaign=share-link'
+}
+
+New-ModuleManifest @moduleSettings -Path './chocolatey-ps-validator.psd1'
+
---
+++ Validator/chocolatey-ps-validator.psm1
@@ -0,0 +1,19 @@
+# Load each scripts in the following folders
+
+$functionFolders = @('functions')
+ForEach ($folder in $functionFolders)
+{
+    $folderPath = Join-Path -Path $PSScriptRoot -ChildPath $folder
+    If (Test-Path -Path $folderPath)
+    {
+        Write-Verbose -Message "Importing from $folder"
+        $functions = Get-ChildItem -Path $folderPath -Filter '*.ps1'
+        ForEach ($function in $functions)
+        {
+            Write-Verbose -Message "  Importing $($function.BaseName)"
+            . $function.FullName
+        }
+    }
+}
+$publicFunctions = (Get-ChildItem -Path "$PSScriptRoot\functions" -Filter '*.ps1').BaseName
+Export-ModuleMember -Function $publicFunctions

33419fe8333247b00c7f16d6cc01699fd2d1d555
Tunisiano187 <fabian@bowlman.org>
2020-06-28 20:08:06 +0200

    Parameters

--- Setup.ps1
+++ Setup.ps1
@@ -2,16 +2,12 @@
 $moduleSettings = @{
     ModuleVersion = '0.0.1'
     Author = 'tunisiano'
-    CompanyName = ''
+    CompanyName = 'None'
     Copyright = '@tunisiano187'
     Description = 'This package will try to give an easy way to check AU package before pack and publishing'
     FunctionsToExport = @()
     AliasesToExport = @()
-    PrivateData = @{
-        PSData = @{
-            ProjectUri = 'https://github.com/tunisiano187/chocolatey-ps-validator'
-        }
-    }
+    ProjectUri = 'https://github.com/tunisiano187/chocolatey-ps-validator'
     HelpInfoURI = 'https://gitter.im/Tunisiano18/community?utm_source=share-link&utm_medium=link&utm_campaign=share-link'
 }
 
